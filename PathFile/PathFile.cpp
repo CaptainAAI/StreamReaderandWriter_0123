db56bef9284428031adc9da748d78c26cbb240ea
CaptainAAI <anandaagungismail@gmail.com>
2024-06-03 10:07:59 +0700

    input file name

--- PathFile/PathFile.cpp
+++ PathFile/PathFile.cpp
@@ -2,3 +2,13 @@ using namespace std;
 #include<iostream>
 #include<fstream>
 #include<string>
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukkan Nama File : ";
+	cin >> NamaFile;
+
+
+}
