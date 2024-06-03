69d1aae73495c5b25bc4a7ddcf25f4778eefbae5
CaptainAAI <anandaagungismail@gmail.com>
2024-06-03 10:34:32 +0700

    try

--- exceptionManual/exceptionManual.cpp
+++ exceptionManual/exceptionManual.cpp
@@ -2,3 +2,12 @@
 #include<exception>
 #include<array>
 using namespace std;
+
+int main()
+{
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << nedl;
+		cout << "pernyataan tidak akan dieksekusi" << endl;
+	}
+
+}
