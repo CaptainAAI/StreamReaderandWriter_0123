414bb449353a75285f1e5d8d0cd065d71a8f3433
CaptainAAI <anandaagungismail@gmail.com>
2024-06-03 10:21:23 +0700

    close file

--- PathFile/PathFile.cpp
+++ PathFile/PathFile.cpp
@@ -30,4 +30,27 @@ int main() {
 	//selesai dalam menulis sekarang  tutup filenya
 	outfile.close();
 
+	//membuuka file dalam mode membaca
+	ifstream infile;
+	
+	//menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiapp baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << "\n";
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+
 }
